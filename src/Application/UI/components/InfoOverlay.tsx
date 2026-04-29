491a3659213f94a1aa71babe15075668b896e58c
demychal <demychals@icloud.com>
2026-04-29 02:58:14 -0500

    changes

--- src/Application/UI/components/InfoOverlay.tsx
+++ src/Application/UI/components/InfoOverlay.tsx
@@ -7,7 +7,7 @@ interface InfoOverlayProps {
 }
 
 const NAME_TEXT = 'тёнь';
-const TITLE_TEXT = 'soon';
+const TITLE_TEXT = 'coming soon';
 const MULTIPLIER = 1;
 
 const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {

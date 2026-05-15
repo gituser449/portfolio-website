f8d0f50051d9ca32555447f84dc34a0da10ef4b5
demychal <demychals@icloud.com>
2026-05-15 01:00:52 -0500

    changes

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://v0-interactive-desktop-website.vercel.app/';
+        iframe.src = 'https://www.shadow.community/';
         /**
          * Use dev server is query params are present
          *
